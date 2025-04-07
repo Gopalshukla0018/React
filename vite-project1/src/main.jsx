3532334bd8371cb85f1861a454130728cb5d5f7a
Gopashukla0018 <gopalhukla0018@gmail.com>
2025-04-08 00:54:32 +0530

    lrarning react poject 2

--- vite-project1/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Navbar from './componants/navbar'
-
-createRoot(document.getElementById('root')).render(
-  
-  <Navbar/>
-)
