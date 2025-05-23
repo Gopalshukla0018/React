0453250238d76c55cfd674d0cad0d9c6375fb8a4
Gopal Shukla <gopalshukla0018@gmail.com>
2025-05-23 16:58:53 +0530

    understandng key,props

--- 18 Fragements/learning-fragement/src/App.jsx
+++ 18 Fragements/learning-fragement/src/App.jsx
@@ -1,19 +1,17 @@
+import ErrorMesage from "./components/Errormessage";
+import FoodItems from "./components/Fooditems";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  // let foodItems = [];
-  let foodItems = ["Dal", "ghee", "green veg", "Roti", "Chana", "peanut"];
+  let foodItems = ["Daal", "ghee", "green veg", "Roti", "Chana", "peanut"];
   return (
     <>
       <h1>Healthy food</h1>
-      {foodItems.length === 0 && <h3>i am still hungry bro </h3>}
-      <ul className="list-group">
-        {foodItems.map((items) => (
-          <li key={items} className="list-group-item">
-            {items}
-          </li>
-        ))}
-      </ul>
+
+      <ErrorMesage items={foodItems} />
+      <FoodItems items={foodItems} />
     </>
   );
 }
---
+++ 18 Fragements/learning-fragement/src/components/Errormessage.jsx
@@ -0,0 +1,5 @@
+const ErrorMesage = (items) => {
+  return <> {items.length === 0 && <h3>i am still hungry bro </h3>}</>;
+};
+
+export default ErrorMesage;
