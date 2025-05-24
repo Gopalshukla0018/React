97cdf47565cde96444116ec275f726645a9029cb
Gopal Shukla <gopalshukla0018@gmail.com>
2025-05-24 16:34:46 +0530

    Passing childred to a container

--- 18 Fragements/learning-fragement/src/App.jsx
+++ 18 Fragements/learning-fragement/src/App.jsx
@@ -1,17 +1,22 @@
 import ErrorMesage from "./components/Errormessage";
 import FoodItems from "./components/Fooditems";
-
+import Container from "./components/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let foodItems = ["Daal","ghee", "green veg", "Roti", "Chana", "peanut"];
+  let foodItems = ["Daal", "ghee", "green veg", "Roti", "Chana", "peanut"];
   return (
     <>
+    <Container>
       <h1 className="food-heading">Healthy food</h1>
 
       <ErrorMesage items={foodItems} />
       <FoodItems items={foodItems} />
+    </Container>
+    <Container>
+      <p>Abve is the list o healthy food that are good fr your health</p>
+    </Container>
     </>
   );
 }
