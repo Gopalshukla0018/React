9aaca2aa783525e113bd221d42a682b7546d6082
Gopal Shukla <gopalshukla0018@gmail.com>
2025-06-18 15:17:47 +0530

    add filter options

--- Projects/FoodDrop/src/App.jsx
+++ Projects/FoodDrop/src/App.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import Header from "./Components/Header";
+import {Header} from "./Components/Header";
 import Body from "./Components/Body";
 import "./App.css";
-
 
 function App() {
   return (
--- Projects/FoodDrop/src/Components/Body.jsx
+++ Projects/FoodDrop/src/Components/Body.jsx
@@ -1,14 +1,44 @@
 import RestaurentCard from "./RestaurentCard";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 // This is the body component which contains the search bar and restaurant cards
 
 const Body = () => {
+  const [Listofrestaurant, setListofrestaurant] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    setListofrestaurant(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  if(Listofrestaurant.length === 0) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div className="body">
-      <div className="search-bar">search</div>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredList = Listofrestaurant.filter(
+            (res) => res.info.avgRating > 4.5
+          );
+          setListofrestaurant(filteredList);
+        }}
+      >
+        Filter
+      </button>
       <div className="res-container">
         {/* restaurent card */}
-        {resList.map((restaurant) => (
+        {Listofrestaurant.map((restaurant) => (
           <RestaurentCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
